c6be245c1f9f2f90709a596d2e8a6f46921d5d57
JulianRamirez07 <jeramirez2681@misena.edu.co>
2024-03-27 16:28:04 -0500

    Completed all migrations for the API

--- database/migrations/2024_03_27_205333_create_series_table.php
+++ database/migrations/2024_03_27_205333_create_series_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('series', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->unsignedBigInteger('role_id');
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('description');
+            $table->year('start_year');
+            $table->year('end_year');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('series');
     }
 };
