a8f97d0f6da4832445442737b35a2fb2421b0099
JulianRamirez07 <jeramirez2681@misena.edu.co>
2024-03-28 14:47:21 -0500

    Finalized route construction

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,35 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\RatingController;
+use App\Http\Controllers\ReductionController;
+use App\Http\Controllers\SerieController;
+// use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// Route to login (accessible to everyone)
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Routes protected by authentication
+Route::middleware(['auth:sanctum'])->group(function () {
+    // Resource routes for ratings and comments
+    Route::resource('/rating', RatingController::class);
+    Route::resource('/comment', CommentController::class);
+
+    // Routes to list movies and series
+    Route::get('/movies/alphabetical', [MovieController::class, 'getMoviesAlphabetically']);
+    Route::get('/movies/sorted-by-rating', [MovieController::class, 'getMoviesSortedByRating']);
+    Route::get('/series/alphabetical', [SerieController::class, 'getSeriesAlphabetically']);
+    Route::get('/series/sorted-by-rating', [SerieController::class, 'getSeriesSortedByRating']);
 });
+
+// Routes that should only be accessible by administrators
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::resource('/movie', MovieController::class);
+    Route::resource('/serie', SerieController::class);
+});
+
+// Extra endpoint
+Route::get('/reduce-string', [ReductionController::class, 'reduceString']);
