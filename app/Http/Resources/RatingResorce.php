329d28da19398a863c58a08436d0aa4181ff17af
JulianRamirez07 <jeramirez2681@misena.edu.co>
2024-03-28 14:46:43 -0500

    Implemented all necessary resource classes

---
+++ app/Http/Resources/RatingResorce.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RatingResorce extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'user' => $this->user->name,
+            'movie' => $this->movie_id ? $this->movie->title : null,
+            'serie' => $this->serie_id ? $this->serie->title : null,
+            'rating' => $this->rating
+        ];
+    }
+}
