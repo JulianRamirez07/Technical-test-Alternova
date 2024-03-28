7f8e87994515277ceaaef108e6482ea17e750e5a
JulianRamirez07 <jeramirez2681@misena.edu.co>
2024-03-28 16:11:47 -0500

    Updated view to display deployed and operational API

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,16 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>API Technical Test - Alternova</title>
 </head>
 <body>
-    @foreach ($roles as $role)
+    {{-- @foreach ($roles as $role)
     <h1>Users with role {{ $role->name }}:</h1>
     <ul>
         @foreach ($role->users as $user)
             <li>{{ $user->name }} {{ $user->email }}</li>
         @endforeach
     </ul>
-    @endforeach
+    @endforeach --}}
+    <div style="font-family: Arial, sans-serif; text-align: center; padding: 20px; background-color: #f2f2f2;">
+        <h1 style="font-size: 24px; color: #333;">API Technical Test - Alternova</h1>
+        <p style="font-size: 16px; color: #666;">by Julian Ramirez</p>
+        <p style="font-size: 14px; color: #999;">IS RUNNING</p>
+    </div>
 </body>
 </html>
